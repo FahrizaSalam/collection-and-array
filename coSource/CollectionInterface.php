3f77c03c927f8cc6c208777e13773d1b7f35790a
FahrizaSalam <fahrizasallam@gmail.com>
2025-11-08 09:11:17 +0700

    Delete coSource directory

--- coSource/CollectionInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-interface CollectionInterface {
-    public function add($element);
-    public function remove($element);
-    public function contains($element);
-    public function size();
-    public function isEmpty();
-    public function clear();
-}
